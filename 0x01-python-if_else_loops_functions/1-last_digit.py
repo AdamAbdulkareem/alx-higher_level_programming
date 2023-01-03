2ef7c979f94b9d5baa1d5b3326bed29f37747ba1
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-03 06:55:27 -0800

    Modify 1-last_digit.py file

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,22 +1,42 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
 last_number = int(str(number)[-1:])
 
 if number < 0:
-     absolute_number = abs(number)
-     negative_last_number = last_number * -1
-     if negative_last_number > 5:
-          print("Last digit of {0} is {1} and is greater than 5".format(number, negative_last_number))
-     elif negative_last_number == 0:
-          print("Last digit of {0} is {1} and is 0".format(number, negative_last_number))
-     else:
-          print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, negative_last_number))
+    absolute_number = abs(number)
+    negative_last_number = last_number * -1
+    if negative_last_number > 5:
+        print(
+            "Last digit of {0} is {1} and is greater than 5".format(
+                number, negative_last_number
+            )
+        )
+    elif negative_last_number == 0:
+        print("Last digit of {0} is {1} and is 0".format(
+                number, negative_last_number
+            )
+        )
+    else:
+        print(
+            "Last digit of {0} is {1} and is less than 6 and not 0".format(
+                number, negative_last_number
+            )
+        )
 
 else:
-     if last_number > 5:
-          print("Last digit of {0} is {1} and is greater than 5".format(number, last_number))
-     elif last_number == 0:
-          print("Last digit of {0} is {1} and is 0".format(number, last_number))
-     else:
-          print("Last digit of {0} is {1} and is less than 6 and not 0".format(number, last_number))
+    if last_number > 5:
+        print(
+            "Last digit of {0} is {1} and is greater than 5".format(
+                number, last_number
+            )
+        )
+    elif last_number == 0:
+        print("Last digit of {0} is {1} and is 0".format(number, last_number))
+    else:
+        print(
+            "Last digit of {0} is {1} and is less than 6 and not 0".format(
+                number, last_number
+            )
+        )
