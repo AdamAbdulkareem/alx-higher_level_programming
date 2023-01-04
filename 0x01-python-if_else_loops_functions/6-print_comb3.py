ddcf551161ef077161c59336afeebf2b59393e46
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-04 03:03:26 -0800

    Modify 6-print_comb3.py file

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for i in range(0, 10):
     for k in range(i + 1, 10):
         if i != k:
             if (int(str(i) + str(k))) == 89:
-                print(str(i) + str(k), end="\n")
+                print("{}".format(str(i) + str(k)), end="\n")
             else:
-                print(str(i) + str(k), end=", ")
+                print("{}".format(str(i) + str(k)), end=", ")
