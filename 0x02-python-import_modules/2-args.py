973e1a07726dd5761ead3c0d0e91828f8a2c06e3
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-05 06:06:51 -0800

    Modify 2-args.py file

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 import sys
 
-num_arguments = len(sys.argv) - 1
-array_arguments = sys.argv
 if __name__ == "__main__":
-    if num_arguments == 0:
-        print("{} arguments.".format(num_arguments), end="\n")
+    if (len(sys.argv) - 1) == 0:
+        print("{} arguments.".format((len(sys.argv) - 1)), end="\n")
     else:
-        print("{} arguments:".format(num_arguments), end="\n")
-        for i in range(1, num_arguments + 1):
-            print("{}: {}".format(i, array_arguments[i]))
+        print("{} arguments:".format((len(sys.argv) - 1)), end="\n")
+        for i in range(1, len(sys.argv)):
+            print("{}: {}".format(i, sys.argv[i]))
