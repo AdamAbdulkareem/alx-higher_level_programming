dbd7876c340091f9ef724bae4b7fd12bc74f3727
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-23 04:25:25 -0800

    Modfy 0-safe_print_list.py file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,7 +9,6 @@ def safe_print_list(my_list=[], x=0):
             else:
                 continue
         print(end="\n")
-    except:
-        print("Value error")
-    finally:
-        return count
+    except ValueError:
+        return
+    return count
