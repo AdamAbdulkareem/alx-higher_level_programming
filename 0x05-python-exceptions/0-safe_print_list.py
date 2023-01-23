ec2a3965ae22bc226f59dd9130a75efde814f300
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-23 03:25:57 -0800

    Modify 0-safe_print_list.py file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for element in my_list:
+            if count != x:
+                print("{}".format(element), end="")
+                count = count + 1
+            else:
+                continue
+        print(end="\n")
+    except:
+        print("Value error")
+    finally:
+        return (count)
