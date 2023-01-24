bfdfe8f6e085b78d9940f1912a50b8018e610823
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-24 08:13:26 -0800

    Modify 2-square.py file

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+        Args:
+            size(int): The size of the new square
+        """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
