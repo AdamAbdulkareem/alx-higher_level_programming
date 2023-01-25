9b1df54fcac4cd5e77f9ec1a680a1f6c23bf3ebc
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-25 01:36:24 -0800

    Modify 4-square.py file

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -22,17 +22,19 @@ class Square:
             The return value is the area of a square.
         """
         return int(self.__size) * int(self.__size)
+
     @property
     def size(self):
         """Get the size of a square:
-            Return: The size of a square
+        Return: The size of a square
         """
         return self.__size
+
     @size.setter
     def size(self, value):
         """Set the size of a square"""
-        if (type(value) != int):
+        if type(value) != int:
             raise TypeError("size must be an integer")
-        if (value < 0):
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
