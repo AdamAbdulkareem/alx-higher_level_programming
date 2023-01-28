22f037a911edf3b8530f5113edc15a9911bf6bb1
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-01-28 10:18:56 -0800

    Modify 0-add_integer.py file

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
+"""Addition module 
+Args:
+    param1 (int): The first parameter
+    param2 (int): Preset parameter
+"""
 def add_integer(a, b=98):
-    """A function that adds 2 integer"""
-    if (type(a) != "int" or "float"):
+    """A function that adds 2 integer
+    Returns: The addition of 2 integers
+    """
+    if (type(a) != int and type(a) == float):
+        a = int(a)
+    if (type(b) != int and type(b) == float):
+        b = int(b)
+    if (type(a) != int and type(a) != float):
         raise TypeError("a must be an integer")
-    if (type(b) != "int" or "float"):
+    if (type(b) != int and type(b) != float):
         raise TypeError("b must be an integer")
-    cast_int(a) = int(a)
-    cast_int(b) = int(b)
-    return cast_int(a) + cast_int(b) 
+    return a + b
