e0d9dc2091155a9a5bc5c8175d47f90a0bdb3e5d
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-03 00:51:04 -0800

    Modify 3-say_my_name.py file

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,4 +5,4 @@ def say_my_name(first_name, last_name=""):
     if type(last_name) != str:
         raise TypeError("last_name must be a string")
     printName = "My name is {} {}".format(first_name, last_name)
-    print(printName)
+    return print(printName)
