d5911e12f97985afe0836b2f63261470a7173805
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-06 05:17:58 -0800

    Modify 2-is_same_class.py file

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """A parent class that inherits child class"""
-list = []
-class MyList(list):
+class MyList:
     def __init__(self, list):
         self.list = list
 
-    def print_sorted(self, list):
-        self.list = self.list.sort()
+    def print_sorted(self):
+        self.list.sort()
         return self.list
