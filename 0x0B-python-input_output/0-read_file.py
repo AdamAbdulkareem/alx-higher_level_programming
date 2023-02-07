6de6306747c761e75219c53fd9e57a27f22d17d6
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-07 08:36:20 -0800

    Modify 0-read_file.py file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function that reads a text file and prints it to stdout"""
+def read_file(filename=""):
+    """Read file with provided filename or path"""
+    with open(filename, encoding="utf-8") as text_file:
+        read_file = text_file.read()
+        print(read_file, end="")
+        return
