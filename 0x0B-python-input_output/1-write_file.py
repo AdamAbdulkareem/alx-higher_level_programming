c10c3b3763baeeb2b8d257bbd38f82e34e1ee8b6
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-07 09:30:27 -0800

    Modify 1-write_file.py file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """Write to file at the provided filename or path"""
-    with open(filename, "w", encoding="utf-8") as text_file:
+    with open(filename, 'w', encoding="utf-8") as text_file:
         write_text = text_file.write(text)
         return write_text
