416a99800e212d81615022cad2642719a8443ecb
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-07 09:23:57 -0800

    Modify 1-write_file.py file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
-    with open(filename,"w", encoding="utf-8") as text_file:
+    with open(filename, "w", encoding="utf-8") as text_file:
         write_text = text_file.write(text)
         return write_text
