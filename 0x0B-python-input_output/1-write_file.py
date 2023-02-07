b3d12636891a527ccdfb3209a7183ff4a5f82811
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-07 09:26:30 -0800

    Mdify 1-write_file.py file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""A function that writes a string to a text file and returns the number of character"""
+
+
 def write_file(filename="", text=""):
+    """Write to file at the provided filename or path"""
     with open(filename, "w", encoding="utf-8") as text_file:
         write_text = text_file.write(text)
         return write_text
