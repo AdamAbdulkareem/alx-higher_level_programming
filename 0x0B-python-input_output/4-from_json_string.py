fda8e055e190300d46e7032adc665d3a7b02f824
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-08 02:12:04 -0800

    Modify 4-from_json_string.py file

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """Import a JSON module"""
 import json
+
 """A function that returns an object represented by a JSON string"""
+
+
 def from_json_string(my_str):
     """JSON representation of my_str
     Return:
