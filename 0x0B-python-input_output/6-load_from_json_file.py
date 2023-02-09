a3f07b294d1d387dcec7fbfff50df224c519fcb3
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-09 04:41:18 -0800

    Modify 6-load_from_json_file.py file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Import a JOSN module"""
+import json
+"""A function that creates an object from a JSON file"""
+def load_from_json_file(filename):
+    """Create Object from provided filename or path"""
+    with open(filename, encoding="utf-8") as json_file:
+        display_file = json_file.read()
+        create_obj = json.loads(display_file)
+        return create_obj
