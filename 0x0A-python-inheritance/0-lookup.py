d9cf93ca05b5feee9157f354a0b209f1855f2ec2
AdamAbdulkareem <dmabdulkareem@gmail.com>
2023-02-22 23:07:05 -0800

    Modify files

--- 0x0A-python-inheritance/0-lookup.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def lookup(obj):
-    """A function that print the list of attributes and methods of an object
-    Args:
-        obj (class): The class object
-    Return: List of atrributes and methods
-    """
-    return dir(obj)
--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""A parent class that inherits child class"""
-class MyList:
-    def __init__(self, list):
-        self.list = list
-
-    def print_sorted(self):
-        self.list.sort()
-        return self.list
