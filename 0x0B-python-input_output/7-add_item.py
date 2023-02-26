9dae3642bc108d6a68186c0b6fc38108c2667db3
Adam Abdulkareem <dmabdulkareem@gmail.com>
2023-02-26 20:06:59 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,36 +1,17 @@
 #!/usr/bin/python3
-"""Import a SYS module"""
+"""This module adds all arguments to a Python list and save them to a file."""
+
+
 import sys
-"""Import a JSON module"""
-import json
 
-"""A function that creates an object from a JSON file"""
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-
-def load_from_json_file(filename):
-    """Create Object from provided filename or path"""
-    with open(filename, encoding="utf-8") as json_file:
-        display_file = json_file.read()
-        create_obj = json.loads(display_file)
-        return create_obj
-
-
-"""A function that writes an object to a text file using JSON representation"""
-
-
-def save_to_json_file(my_obj, filename):
-    """Write the filename or path provided by the function
-    Return:
-        Text written to the provided filename or path
-    """
-    with open(filename, "w", encoding="utf-8") as text_file:
-        json_object = json.dumps(my_obj)
-        write_file = text_file.write(json_object)
-        return write_file
-
-
-for items in range(1, len(sys.argv)):
-    display_argv = sys.argv[items]
-    display_list = load_from_json_file("add_item.json")
-    append_display_list = display_list.append(display_argv)
-    save_to_json_file(append_display_list, "add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
