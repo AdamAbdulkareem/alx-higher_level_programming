cc1d38d0d6b772a74f181044ec7c0b600e038637
adamabdulkareem <dmabdulkareem@gmail.com>
2023-06-14 11:00:47 -0700

    Modify 2-arguments.js file

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
-if (args.length == 3) {
+if (args.length === 3) {
   console.log('Argument found');
-}
-else if (args.length >= 4) {
-    console.log('Arguments found');
-}
-else {
+} else if (args.length >= 4) {
+  console.log('Arguments found');
+} else {
   console.log('No argument');
 }
