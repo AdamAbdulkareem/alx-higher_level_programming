2e521ce430b391ccb2459a9410630fe8384bf11c
adamabdulkareem <dmabdulkareem@gmail.com>
2023-06-15 07:25:53 -0700

    Modify 8-square.js file

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,18 +1,13 @@
 #!/usr/bin/node
-let count = process.argv[2];
-if (isNaN(Math.floor(Number(count)))){
-    console.log('Missing size');
-}
-else if (Number(count) <=0){
-    return;
-}
-else{
-   for (let i=0; i < count; i++){
-      let result = '';
-      for (let j=0; j < count; j++){
-        result += 'X';
-       }
+const count = process.argv[2];
+if (isNaN(Math.floor(Number(count)))) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < count; i++) {
+    let result = '';
+    for (let j = 0; j < count; j++) {
+      result += 'X';
+    }
     console.log(result);
-   }
+  }
 }
-
