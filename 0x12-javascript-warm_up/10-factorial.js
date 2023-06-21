1f6495a51ad72a3b44b41bda1dc869aeef01682a
adamabdulkareem <dmabdulkareem@gmail.com>
2023-06-21 00:27:37 -0700

    Modify 10-factorial file

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = parseInt(process.argv[2]);
 function factorial (num) {
-  if (num < 0) { return -1; } else if (num === 0) { return 1; } else {
+  if (num < 0) { return -1; } else if (num === 0) { return 1; } else if (isNaN(num)) { return 1; } else {
     return (num * factorial(num - 1));
   }
 }
