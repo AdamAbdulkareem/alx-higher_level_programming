f65214ea83fd611b0d0bb79059332f851f885f7b
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-06 11:48:40 -0700

    Modify 0-hbtn_status.py file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-from urllib.request import urlopen
+"""
+This script retrieves the content of a URL and print various details about the response
+"""
 
 if __name__ == "__main__":
-    """
-    This script retrieves the content of a URL and print various details about the response
-    """
-    with urlopen("https://alx-intranet.hbtn.io/status") as response:
+    import urllib.request
+
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
-    print("Body response:")
-    print(f"    - type: {type(body)}")
-    print(f"    - content: {body}")
-    print(f"    - utf8 content: {body.decode('utf-8')}")
+        print("Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        print(f"    - utf8 content: {body.decode('utf-8')}")
