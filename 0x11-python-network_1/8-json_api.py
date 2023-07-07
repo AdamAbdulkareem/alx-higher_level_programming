35ad8d36cfbaa36d284d002fc95bf03e1a110383
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-07 09:32:10 -0700

    Modify 8-json_api.py file

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This script takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = sys.argv[1]
+    data = {"letter": q}
+    request = requests.post(url, data)
