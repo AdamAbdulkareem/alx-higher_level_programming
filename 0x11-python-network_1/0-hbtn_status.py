6331d3572df6eb037ec3c84fd2b389e710dc11bc
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-07 04:16:29 -0700

    Modify some files

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
+
 """
-A script that fetches https://intranet.hbtn.io/status.
+a Python script that fetches https://alx-intranet.hbtn.io/status
 """
 
+import urllib.request
 
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        content = response.read()
-        print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as r:
+        content = r.read()
+        print('Body response:')
+        print(f'\t- type: {type(content)}')
+        print(f'\t- content: {content}')
+        print(f'\t- utf8 content: {content.decode("utf-8")}')
