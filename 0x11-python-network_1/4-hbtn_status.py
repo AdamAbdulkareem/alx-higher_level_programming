804c5cba14a0e651a16ecf0cfe15683f28f3cc3f
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-07 05:29:55 -0700

    Modify 4-hbtn_status.py file

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,12 +2,12 @@
 """
 This script fetches https://alx-intranet.hbtn.io/status
 """
-import urllib.request
+import requests
 
 url = " https://alx-intranet.hbtn.io/status"
-request = urllib.request.Request(url)
-with urllib.request.urlopen(url) as response:
-    content = response.read().decode("utf-8")
-    print("Body response:")
-    print(f"\t- type: {type(content)}")
-    print(f"\t- content: {content}")
+response = requests.get(url)
+
+content = response.text
+print("Body response:")
+print(f"\t- type: {type(content)}")
+print(f"\t- content: {content}")
