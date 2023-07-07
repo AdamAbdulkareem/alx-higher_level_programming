384c0dfdaae22be75ebfc657c083c1257fd34f51
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-07 04:50:23 -0700

    Modify 3-error_code.py file

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -8,12 +8,13 @@ import sys
 import urllib.request
 import urllib.error
 
-url = sys.argv[1]
-request = urllib.request.Request(url)
-try:
-    with urllib.request.urlopen(request) as response:
-        content = response.read()
-        print(content.decode('utf-8'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            content = response.read().decode("utf-8")
+            print(content)
 
-except urllib.error.HTTPError as e:
-    print(f"Error code: {e.code}")
+    except urllib.error.HTTPError as e:
+        print(f"Error code: {e.code}")
