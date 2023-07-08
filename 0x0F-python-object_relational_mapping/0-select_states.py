2e6a9cc0745a4104f754be00c562beb27c4c9957
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-08 06:10:05 -0700

    Modify some files

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
 This script lists all staes from the database hbtn_0e_0_usa
-:"""
+"""
 import MySQLdb
 import sys
 """
 Import the MySQLdb and sys module
 """
-username = sys.argv[1]
-password = sys.argv[2]
-db_name = sys.argv[3]
 
 if __name__ == "__main__":
     """
     A connection is established to the MySQL database
     """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
     db = MySQLdb.connect(
         host="localhost",
         user=username,
