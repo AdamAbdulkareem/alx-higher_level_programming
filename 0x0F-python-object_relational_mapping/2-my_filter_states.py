b1602ebbf6cbc57853818a77614f01152609269c
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-08 08:07:50 -0700

    Modify 2-my_filter_states.py file

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This script lists all staes from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+"""
+Import the MySQLdb and sys module
+"""
+
+if __name__ == "__main__":
+    """
+    A connection is established to the MySQL database
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_searched = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost", user=username, port=3306, passwd=password, db=db_name
+    )
+    cursor = db.cursor()
+    """
+    Create the cursor object
+    """
+    query = "SELECT * FROM states WHERE name = '{}' \
+        ORDER BY states.id ASC".format(
+        state_searched
+    )
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
