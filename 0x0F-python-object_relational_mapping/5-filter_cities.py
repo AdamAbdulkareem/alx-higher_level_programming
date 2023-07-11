01cdc760fb8a7fa8b15af94293a7ba9b9f163b93
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-11 03:44:20 -0700

    Modify 5-filter_cities.py file

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_4_usa
+"""
+import MySQLdb
+import sys
+
+"""
+Import the MySQLdb and sys module
+"""
+
+if __name__ == "__main__":
+    """
+    A connection is established to the MySQL database
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost", user=username, port=3306, passwd=password, db=db_name
+    )
+    cursor = db.cursor()
+    """
+    Create the cursor object
+    """
+    query = "SELECT cities.name FROM cities \
+        INNER JOIN states ON cities.state_id = states.id \
+        WHERE states.name = %s ORDER BY state_id ASC"
+    cursor.execute(query, (state_name,))
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+    cursor.close()
+    db.close()
