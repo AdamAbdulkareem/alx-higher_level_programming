055c488aef866b11b7d2abf22aa5608387e7d43d
adamabdulkareem <dmabdulkareem@gmail.com>
2023-07-12 02:22:56 -0700

    Modify model_state.py file

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,19 +6,23 @@ and an instance Base = declarative_base():
 
 from sqlalchemy import create_engine, Column, Integer, String, Sequence
 from sqlalchemy.ext.declarative import declarative_base
+
 """Import SQLAlchemy"""
 
 Base = declarative_base()
 """Define the database models"""
 
+
 class State(Base):
     """The State class that inherits from base
-       Args:
-           id: Auto generate, unique, primary key, can't be null
-           name: String, can't be null
+    Args:
+        id: Auto generate, unique, primary key, can't be null
+        name: String, can't be null
     """
 
-    __tablename__ = 'states'
-    id = Column(Integer, Sequence('state_id_seq'), unique=True,
-                nullable=False, primary_key=True)
+    __tablename__ = "states"
+    id = Column(
+        Integer, Sequence("state_id_seq"), unique=True,
+        nullable=False, primary_key=True
+    )
     name = Column(String(128), nullable=False)
