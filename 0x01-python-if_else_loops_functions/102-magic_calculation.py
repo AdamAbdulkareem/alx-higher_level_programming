c05a5f6ea3f9bd6595d233c0884221d64cec83b9
adamabdulkareem <dmabdulkareem@gmail.com>
2023-08-27 12:11:59 -0700

    Modify some files

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    return print(dis.dis(magic_calculation))
-magic_calculation(a, b, c)
+    if (a < b):
+        return (c)
+    elif (c > b):
+        return (a + b)
+    else:
+        return (a * b - c)
