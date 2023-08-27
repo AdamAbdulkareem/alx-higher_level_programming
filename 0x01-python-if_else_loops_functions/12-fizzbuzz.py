da4674f6ccb407bba532deb1446eb2945a863111
adamabdulkareem <dmabdulkareem@gmail.com>
2023-08-27 04:32:23 -0700

    Modify some files

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    print(
-        *map(lambda i: "Fizz" * (not i % 3) + "Buzz" * (not i % 5) or i, range(1, 101)),
-        sep=" "
-    )
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end='')
+        elif i % 3 == 0:
+            print("Fizz", end='')
+        elif i % 5 == 0:
+            print("Buzz", end='')
+        else:
+            print(i, end='')
+
+        print(" ", end='')
